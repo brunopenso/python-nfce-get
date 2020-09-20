723d0404ed732250851614fb2c0bc1e79cace798
brunopenso <viperp@gmail.com>
2020-09-19 22:30:56 -0300

    move index.py

---
+++ src/index.py
@@ -0,0 +1,15 @@
+import urllib3
+listOfStatesAvailableQrCode = ['pr.gov.br/nfce/qrcode']
+
+def getJsonFromQrcodelink(link):
+    for states in listOfStatesAvailableQrCode:
+        if (states in link):
+            http = urllib3.PoolManager()
+            htmlData = http.request('GET', link).data
+            return nfceHtml.getJsonFromHtml(htmlData)
+    raise StateInvalid('According to link provided the state related to the domain is not available')
+
+def getJsonFromFile(filePath):
+    f = open(filePath, 'r')
+    htmlData = f.read()
+    return nfceHtml.getJsonFromHtml(htmlData)
