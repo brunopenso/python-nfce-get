791bbee383b54f0722734efe91d469bb0977ea86
brunopenso <viperp@gmail.com>
2020-09-19 21:55:21 -0300

    State invalid exception

--- index.py
+++ index.py
@@ -1,10 +1,14 @@
-from src import nfceHtml
+from src import nfceHtml, StateInvalid
 import urllib3
+listOfStatesAvailableQrCode = ['pr.gov.br/nfce/qrcode']
 
 def getJsonFromQrcodelink(link):
-    http = urllib3.PoolManager()
-    htmlData = http.request('GET', link).data
-    return nfceHtml.getJsonFromHtml(htmlData)
+    for states in listOfStatesAvailableQrCode:
+        if (states in link):
+            http = urllib3.PoolManager()
+            htmlData = http.request('GET', link).data
+            return nfceHtml.getJsonFromHtml(htmlData)
+    raise StateInvalid('According to link provided the state related to the domain is not available')
 
 def getJsonFromFile(filePath):
     f = open(filePath, 'r')
---
+++ src/StateInvalid.py
@@ -0,0 +1,8 @@
+class StateInvalid(Exception):
+    def __init__(self, message, errors):
+
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+
+        # Now for your custom code...
+        self.errors = errors
