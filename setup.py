c3b3243e872fc6fe7e8c74ce80f1a3e8db32664e
brunopenso <viperp@gmail.com>
2020-09-20 22:03:34 -0300

    better setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,9 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="nfce-get-brunopenso", # Replace with your own username
-    version="0.0.1",
+    name="nfce-get", # Replace with your own username
+    version="1.0.1",
     author="Bruno Penso",
-    author_email="author@example.com",
     description="Pacote para recuperar uma NFCE (nota fiscal consumidor eletronica) e transformar em json.",
     long_description=long_description,
     long_description_content_type="text/markdown",
