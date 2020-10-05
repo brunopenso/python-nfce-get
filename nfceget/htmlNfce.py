18758fa0647116213dab4348dba4252dac4c2d74
brunopenso <viperp@gmail.com>
2020-10-04 21:46:17 -0300

    refactor folder by state

--- nfceget/htmlNfce.py
+++ nfceget/htmlNfce.py
@@ -1,4 +1,8 @@
-from .pr.notaparana import get_json_from_html
+from .pr.notaparana import get_json_from_html as notaparana
+from .pr.receitaestadual import get_json_from_html as receitaestadual
 
-def run(data):
-    return get_json_from_html(data)
+def run(local, data):
+    if (local == 'notaparana'):
+        return notaparana(data)
+    if (local == 'receitaestadualpr'):
+        return receitaestadual(data)
