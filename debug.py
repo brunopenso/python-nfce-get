6fbea4802f2cc509b57e00186833e8e68a27a9ce
brunopenso <viperp@gmail.com>
2021-03-30 22:16:40 -0300

    find qrcode from receitaestadual and parse it

--- debug.py
+++ debug.py
@@ -3,6 +3,6 @@ import pytest
 from nfceget import app
 from nfceget.StateInvalidError import StateInvalidError
 
-data = app.json_from_file('./tests/html/pr/receitaestadual1.html')
-
-print(data)
+#data = app.json_from_qrcode_link('http://www.fazenda.pr.gov.br/nfce/qrcode?p=41201078116670001307650020003062671006208535|2|1|1|9027FAF627D5B3DB0ED7DFACEA932CEC763419EF')
+#data = app.json_from_file('./tests/html/pr/receitaestadual1.html')
+#print(data)
--- nfceget/htmlNfce.py
+++ nfceget/htmlNfce.py
@@ -1,8 +1,12 @@
 from .pr.notaparana import get_json_from_html as notaparana
-from .pr.receitaestadual import get_json_from_html as receitaestadual
+from .pr.receitaestadual import get_qr_code as receitaestadualpr
+import urllib3
 
 def run(local, data):
     if (local == 'notaparana'):
         return notaparana(data)
     if (local == 'receitaestadualpr'):
-        return receitaestadual(data)
+        linkQrCode = receitaestadualpr(data)
+        http = urllib3.PoolManager()
+        html_data = http.request('GET', linkQrCode).data   
+        return notaparana(html_data)
